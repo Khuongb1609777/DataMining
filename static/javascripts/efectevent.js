0eef3621b991d5876aa9ae6e1b3df6e30c32c661
unknown <nguyenkhoa066@gmail.com>
2020-06-01 08:40:22 +0700

    complete theme by Khoa

---
+++ static/javascripts/efectevent.js
@@ -0,0 +1,12 @@
+$(function() {
+    var effects = "animated bounce";
+    var effectsEnd = "animationed oAnimationEnd mozAnimationEnd webkitAnimationEnd";
+
+        $("a.button").hover(function(){
+                $(this).addClass(effects).one(effectsEnd, function(){
+                        $(this).removeClass(effects);
+                });
+
+        });
+
+});
